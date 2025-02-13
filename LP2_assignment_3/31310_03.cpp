7b90039f33e69507f26c9b06aba68d24e72ebc85
ankitbugade <ankitbugade04@gmail.com>
2025-02-13 15:13:51 +0530

    update

--- LP2_assignment_3/31310_03.cpp
+++ LP2_assignment_3/31310_03.cpp
@@ -2,32 +2,32 @@
 using namespace std;
 
 
-// // SELECTION SORT
-// int main(){
-//     vector <int> arr = {4,2,1,11,5,41,55,0,24,15,12,22,4,21};
+// SELECTION SORT
+int main(){
+    vector <int> arr = {4,2,1,11,5,41,55,0,24,15,12,22,4,21};
 
-//     for (int i = 0; i < arr.size()-1; i++)
-//     {
-//         int m = 100;
-//         int ind = -1;
-//         for (int j = i; j < arr.size(); j++)
-//         {
-//             if(arr[j]<m){
-//                 m = arr[j];
-//                 ind = j;
-//             }
-//         }
-//         if(i!=ind)swap(arr[i],arr[ind]);
-//     }
+    for (int i = 0; i < arr.size()-1; i++)
+    {
+        int m = 100;
+        int ind = -1;
+        for (int j = i; j < arr.size(); j++)
+        {
+            if(arr[j]<m){
+                m = arr[j];
+                ind = j;
+            }
+        }
+        if(i!=ind)swap(arr[i],arr[ind]);
+    }
     
-//     for (int i = 0; i < arr.size(); i++)
-//     {
-//         cout<<arr[i]<<" ";
-//     }
-//     cout<<endl;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
     
 
-//     return 0;
+    return 0;
 
-// }
+}
 
